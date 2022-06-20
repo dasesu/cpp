229a97716be5eda9a85f657111db8dab871bbf9b
dasesu <dasesumail@gmail.com>
2022-06-20 10:42:35 -0400

    actualizacion normal sin grandes cambios

--- sorting/01_selection_sort/selection_sort.cpp
+++ sorting/01_selection_sort/selection_sort.cpp
@@ -1,49 +1,47 @@
 #include <iostream>
+#include <pthread.h>
 
-  int selection_sort(int *A, int tam){
-      // Es un algoritmo que tiene una complejidad de tiempo de O(n^2)
-      int pos_min{0}; // almacena la posicion del valor minimo parcial 
-      for( int j=0;j<tam-1;j++){
-         pos_min=j;
-         for(int i=j+1;i<tam;i++){
-            if( A[i] < A[pos_min] ){
-               pos_min = i;
-            }
-         }
-         printf("el minimo actual es: %d\n", A[pos_min]);
-         int aux{}; // variable auxiliar 
-         aux = A[j];
-         A[j] = A[pos_min];
-         A[pos_min] = aux;
+int selection_sort(int *A, int tam) {
+  // Es un algoritmo que tiene una complejidad de tiempo de O(n^2)
+  int pos_min{0}; // almacena la posicion del valor minimo parcial
+  for (int j = 0; j < tam - 1; j++) {
+    pos_min = j;
+    for (int i = j + 1; i < tam; i++) {
+      if (A[i] < A[pos_min]) {
+        pos_min = i;
       }
-      return 0;
-   }
+    }
+    printf("el minimo actual es: %d\n", A[pos_min]);
+    int auxok{}; // variable auxiliar
+    auxok = A[j];
+    A[j] = A[pos_min];
+    A[pos_min] = auxok;
+  }
+  return 0;
+}
 
+int main() {
+  FILE *fp;
+  int val{0};
+  int tam{10000};
+  int A[tam] = {}; // lo mismo que crear un for(int i=0;i<tam;i++){ A[i] = 0; }
+  fp = fopen("file.txt", "r");
+  int i{0};
+  while (i < tam) {
+    fscanf(fp, "%d\n", &val);
+    A[i] = val;
+    i++;
+  }
+  fclose(fp);
 
-   int main(){
-      FILE *fp;
-      int val{0};
-      int tam{10000};
-      int A[tam] = {}; // lo mismo que crear un for(int i=0;i<tam;i++){ A[i] = 0; }
-      fp = fopen("file.txt", "r");
-      int i{0};
-      while(i<tam){
-         fscanf(fp, "%d\n", &val);
-         A[i] = val;
-         i++;
-      }
-      fclose(fp);
-      
-      selection_sort( A, tam );
+  selection_sort(A, tam);
+  fp = fopen("output.txt", "w");
+  i = 0;
+  while (i < tam) {
+    fprintf(fp, "%d\n", A[i]);
+    i++;
+  }
+  fclose(fp);
 
-      fp = fopen("output.txt", "w");
-      i=0;
-      while(i<tam){
-         fprintf(fp,"%d\n", A[i]);
-         i++;
-      }
-      fclose(fp);
-
-      return 0;
-   }
-
+  return 0;
+}
