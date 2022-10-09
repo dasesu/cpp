d2423bd0767dcf84b72459a938925d317f6304fd
dasesu <dasesumail@gmail.com>
2022-10-09 19:45:17 -0400

    subida

---
+++ c++_basico/039.- Curso C++ Básico. Namespaces definidos por el usuario/circulo.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef CIRCULO_H
+#define CIRCULO_H
+
+namespace matBasic
+{
+   // la constante es tambien parte del namespace
+   constexpr double pi{3.14};
+}
+#endif
+
---
+++ c++_basico/039.- Curso C++ Básico. Namespaces definidos por el usuario/crece.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef CRECE_H
+#define CRECE_H
+
+namespace matBasic
+{
+   // la constante es tambien parte del namespace
+   constexpr double e{2.7};
+}
+#endif
+
